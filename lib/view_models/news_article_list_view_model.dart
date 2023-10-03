cc309748c29ed46de254664dc45409ee356d5221
thibaultrousset <thibaultrousset94@gmail.com>
2023-10-03 09:13:49 +0200

    faet: remove unsused constroctor

--- lib/view_models/news_article_list_view_model.dart
+++ lib/view_models/news_article_list_view_model.dart
@@ -6,7 +6,6 @@ import 'package:fresh_news/view_models/news_article_view_model.dart';
 class NewsArticleListViewModel extends ChangeNotifier {
   List<NewsArticleViewModel> articles = <NewsArticleViewModel>[];
 
-  NewsArticleListViewModel();
   void populateTopHeadlines() async {
     List<NewsArticle> newsArticles = await WebService().fetchTopHeadlines();
     articles =
